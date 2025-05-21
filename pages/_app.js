8d41e66e48e3b0a3d6ce6a6f0af63284d5f226e7
capitolstack <jason@capitolstack.vc>
2025-05-20 23:18:03 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Script from 'next/script';
-import '@/styles/globals.css';
+import '../styles/globals.css';
+
 
 export default function App({ Component, pageProps }) {
   return (
