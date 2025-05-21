dfe7a1f99d0fa31b489bce8dfe503fb1a52d7c89
capitolstack <jason@capitolstack.vc>
2025-05-20 21:43:21 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Script from 'next/script';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
@@ -12,21 +13,6 @@ export default function App({ Component, pageProps }) {
         />
         <link rel="icon" href="/favicon.ico" />
 
-            {/* Google Analytics */}
-  <script async src={`https://www.googletagmanager.com/gtag/js?id=G-NH6MMP8EQF`}></script>
-  <script
-    dangerouslySetInnerHTML={{
-      __html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-NH6MMP8EQF', {
-          page_path: window.location.pathname,
-        });
-      `,
-    }}
-  />
-
         {/* Open Graph / Social Preview */}
         <meta property="og:title" content="Capitol Stack – Founder-First Climate Tech VC" />
         <meta
@@ -34,13 +20,33 @@ export default function App({ Component, pageProps }) {
           content="Investing in overlooked but deeply capable builders at the intersection of climate, government, and software."
         />
         <meta property="og:image" content="/images/og-preview.png" />
-        <meta property="og:url" content="https://capitolstack.vercel.app" />
+        <meta property="og:url" content="https://capitolstack.vc" />
         <meta name="twitter:card" content="summary_large_image" />
 
         {/* Mobile UI Color */}
         <meta name="theme-color" content="#007070" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+
+      {/* ✅ Load GA4 Script with Next.js Script */}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-NH6MMP8EQF"
+      />
+      <Script
+        id="ga-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-NH6MMP8EQF', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
 
       <Component {...pageProps} />
     </>
