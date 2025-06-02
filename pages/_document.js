3c07dfe738e0c23cb6ab9be25318e5ac541978aa
capitolstack <jason@capitolstack.vc>
2025-06-02 17:49:20 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,26 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* Google Fonts: Inter */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
-          rel="stylesheet"
-        />
-
-        {/* Google Analytics (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-NH6MMP8EQF"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-NH6MMP8EQF', { debug_mode: true });
-            `,
-          }}
-        />
-      </Head>
+      <Head />
       <body className="font-inter">
         <Main />
         <NextScript />
