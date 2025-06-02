fe856d5e1498198f51cb2c4fbfd0de6b4b6f5f9c
capitolstack <jason@capitolstack.vc>
2025-06-02 17:32:54 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,8 +15,30 @@ module.exports = {
         primary: '#007070'
       },
       fontFamily: {
-        sans: ['Inter', ...fontFamily.sans],
+        sans: ['var(--font-inter)', ...fontFamily.sans],
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.gray.800'),
+            a: {
+              color: theme('colors.primary'),
+              '&:hover': {
+                color: '#005050',
+              },
+            },
+            h1: { fontWeight: '700', letterSpacing: '-0.025em' },
+            h2: { fontWeight: '600', letterSpacing: '-0.02em' },
+            h3: { fontWeight: '600' },
+            strong: { color: theme('colors.gray.900') },
+            code: {
+              backgroundColor: theme('colors.gray.100'),
+              padding: '0.25rem 0.375rem',
+              borderRadius: '0.25rem',
+            },
+          },
+        },
+      }),
     },
   },
   plugins: [
