8c76c909c7ac98c3262fedc76aa3cafd5e12bc5a
capitolstack <jason@capitolstack.vc>
2025-06-03 22:55:55 -0400

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -29,9 +29,9 @@ export default function Blog({ featured, posts }) {
 
       <div className="bg-white dark:bg-gray-900">
         {/* Header */}
-        <div className="mx-auto max-w-7xl px-6 lg:px-8 mt-24">
+        <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8">
           <div className="mx-auto max-w-2xl text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-6xl mt-20">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-6xl">
               Inside Capitol Stack
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-200">
@@ -59,7 +59,7 @@ export default function Blog({ featured, posts }) {
 
         {/* Featured article */}
         {featured && searchTerm === '' && (
-          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8 mt-24">
             <div className="mx-auto max-w-2xl border-t border-gray-200 dark:border-gray-800 pt-16 sm:pt-20">
               <div className="text-center">
                 <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
