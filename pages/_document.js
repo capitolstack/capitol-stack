0f673abd0d263a8582775c23dad26e8085b7c0c0
capitolstack <jason@capitolstack.vc>
2025-06-05 16:29:15 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="font-inter">
+    <Html>
+      <Head>
+        {/* Tally modal embed script */}
+        <script async src="https://tally.so/widgets/embed.js"></script>
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
